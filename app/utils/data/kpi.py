c14e55ec7e5d217fd2884ccc142998a553679113
felipe-massa_TFMC <felipe.massa@technipfmc.com>
2025-12-26 20:31:50 -0300

    Listing KPIs to be created

---
+++ app/utils/data/kpi.py
@@ -0,0 +1,78 @@
+# fixit add KPIs
+
+# fixit implement per-category monthly budget
+
+# List of KPIs to track
+# (consolidate in new screen, not in monthly or yearly view)
+
+# Show in 3 sections (always show the 3 together):
+# - This month
+# - Last 3 months
+# - This year
+# - Enable custom date range picker (last year, last 6 months, last 3 months, last month, this month, this year, all time)
+
+# Show in two flavors:
+# - Projected (default)
+# - Actual
+
+# Define target for each KPI
+# fixit implement target visualization
+
+# General:
+# - Total income
+# - Total expenses
+
+# Net Income:
+# - Net Income Amount (absolute value)
+# - Net Income Percentage (Net Income / Total Income)
+#   - Target: Beginner = 10%-20%, Ideal = 25%-40%, Elite = 50%+
+# - Expenses Inflation
+#   - Income inflation vs expenses inflation (which date range?)
+#   - Target: Beginner = <5%, Ideal = <3%, Elite = <0%
+
+# Income:
+# - Salary increase vs last period (how to measure this? which date range?)
+# - Percentage of fixed income against total income
+
+# Expenses:
+# - Expenses increase vs last period (how to measure this? which date range?)
+#   - Target: ???
+# - Distribution: percentage of fixed expenses vs variable expenses vs lifestyle expenses
+# - Absolute value of fixed expenses
+#   - Target for fixed: Beginner = X%, Ideal = Y%
+# - Absolute value of variable expenses
+#   - Target for variable: Beginner = X%, Ideal = Y%
+# - Absolute value of lifestyle expenses
+#   - Target for lifestyle: Beginner = X%, Ideal = Y%
+# - Percentage of fixed expenses against total income
+#   - Target for fixed: Beginner = X%, Ideal = Y%
+# - Percentage of variable expenses against total income
+#   - Target for variable: Beginner = X%, Ideal = Y%
+# - Percentage of lifestyle expenses against total income
+#   - Target for lifestyle: Beginner = X%, Ideal = Y%
+# - Top 3 fixed expense categories
+# - Top 3 variable expense categories
+# - Top 3 lifestyle expense categories
+# - Expense breakdown by category (absolute values)
+#   - Add expander with full breakdown by category
+#   - Highlight some key categories to track (most actionable ones):
+#     - Home
+#     - Personal Felp
+#     - Personal Lena
+#     - Recreation
+#     - Restaurant
+
+# Travel:
+# - Budget adherence: budgeted vs actual travel expenses
+#   - Target: Beginner = <20%, Ideal = <10%, Elite = <0%
+
+# fixit implement investments KPIs (long-term)
+# fixit implement KPI based on how much of income is invested monthly (long-term)
+# fixit implement "Months of Runway" KPI (long-term)
+# Months of Runway = Emergency Fund / Average Monthly Expenses Over Last 6 Months
+# - Target: Beginner: 3 months, Strong: 6 months, Excellent: 12 months
+# Investments:
+# -
+# -
+# -
+
